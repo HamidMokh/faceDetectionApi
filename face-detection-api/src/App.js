4dee6fc642a7de3b677a6274acf484acecad0733
Hamid Mokhtari <mokhtarihamid44@gmail.com>
2022-07-01 18:17:00 +0100

    testing the addition of the first component

---
+++ face-detection-api/src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import Navigation from './components/Navigation/Navigation';
+import './App.css';
+import { render } from '@testing-library/react';
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+       <Navigation />
+         {/* <Logo />
+        <ImageLinkForm/>
+        <FaceRecognition/> */}
+        
+      </div>
+    );
+  }
+  
+}
+
+export default App;
