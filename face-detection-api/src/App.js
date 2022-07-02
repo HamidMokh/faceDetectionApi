d34b4da77cbbfd0bf3cf9784828444dc195dcc3e
Hamid Mokhtari <mokhtarihamid44@gmail.com>
2022-07-02 02:18:12 +0100

    adding the rank section

--- face-detection-api/src/App.js
+++ face-detection-api/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
 
@@ -12,6 +13,7 @@ class App extends Component{
        <Navigation />
          <Logo />
          <ImageLinkForm/>
+         <Rank/>
          {/*
         <FaceRecognition/> */}
         
