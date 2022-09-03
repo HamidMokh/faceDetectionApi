c585e9ad544fc1dcd328b676821559b6a72be1e2
Hamid Mokhtari <mokhtarihamid44@gmail.com>
2022-09-03 20:16:03 +0100

    adding face detection box

--- face-detection-api/src/App.js
+++ face-detection-api/src/App.js
@@ -104,9 +104,21 @@ class App extends Component{
       box:{},
     }
   }
-  
 
+  calculateFaceLocation =(data) => {
+  const clarifaiFace=  data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width =Number(image.width);
+  const height = Number(image.height);
+  return {
+    leftCol   : clarifaiFace.left_col * width,
+    topRow    : clarifaiFace.top_row * height,
+    rightCol  : width -  (clarifaiFace.right_col*width),
+    bottomRow : height - (clarifaiFace.bottom_row * height),
+  }
+  }
   displayFaceBox = (box) => {
+    console.log(box);
     this.setState({box: box});
   }
 
@@ -121,22 +133,24 @@ class App extends Component{
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
       .then(response => {
-        console.log('hi', response);
-        if (response) {
-          fetch('http://localhost:3000/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              id: this.state.user.id
-            })
-          })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count}))
-            })
+        console.log(response);
+        
+        // if (response) {
+        //   fetch('http://localhost:3000/image', {
+        //     method: 'put',
+        //     headers: {'Content-Type': 'application/json'},
+        //     body: JSON.stringify({
+        //       id: this.state.user.id
+        //     })
+        //   })
+        //     .then(response => response.json())
+        //     .then(count => {
+        //       this.setState(Object.assign(this.state.user, { entries: count}))
+        //     })
 
-        }
+        // }
         this.displayFaceBox(this.calculateFaceLocation(response))
+        
       })
       .catch(err => console.log(err));
    
@@ -157,7 +171,7 @@ class App extends Component{
       loaded={particlesLoaded}
       options={particlesOptions}
     />
-      <FaceRecognition imageUrl={this.state.imageUrl}/>
+      <FaceRecognition box= {this.state.box} imageUrl={this.state.imageUrl}/>
         
       </div>
     );
